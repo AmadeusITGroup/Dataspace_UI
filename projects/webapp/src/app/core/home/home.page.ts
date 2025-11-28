f20b05161c58e29cc30dbdfddbc99eefff57563b
AndreeaCascaval <andreea.cascaval@outlook.com>
2025-11-28 16:45:19 +0100

    update
    update

--- projects/webapp/src/app/core/home/home.page.ts
+++ projects/webapp/src/app/core/home/home.page.ts
@@ -5,18 +5,12 @@ import { LoginService } from '../services/login.service';
 @Component({
   styleUrl: './home.page.scss',
   template: `
-    <div class="px-0 position-relative img-container">
-      <picture>
-        <source type="image/webp" media="(max-width: 1199px)" srcset="assets/bg-eonax.webp" />
-        <source type="image/avif" media="(max-width: 1199px)" srcset="assets/bg-eonax.avif" />
-        <img alt="header image" class="header-img" src="assets/bg-eonax.jpg" priority />
-      </picture>
+    <div class="px-0 position-relative img-container bg-primary-subtle">
       <div
-        class="text-info position-absolute w-100  title-div d-flex flex-column align-items-center gap-4"
-      >
+        class="text-info position-absolute w-100  title-div d-flex flex-column align-items-center gap-4">
         <div class="d-flex flex-column gap-4 align-items-center mt-5">
-          <a href="https://eona-x.eu/">
-            <img src="eona-x.svg" alt="fancy logo" width="196" height="196" />
+          <a href="https://amadeus.com">
+            <img src="1a-full.svg" alt="fancy logo" width="196" height="196" />
           </a>
           <h1 class="text-center">Data sharing that keeps us moving</h1>
         </div>
